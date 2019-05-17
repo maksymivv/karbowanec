9fa5451937de091a203c1160744532da0a7dd696
aiwe <maksymivv@gmail.com>
2019-05-17 23:34:43 +0300

    add iterative part

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -1223,39 +1223,213 @@ void fillHeights(const void* seed, size_t seedSize, uint64_t maxHeight, std::vec
   }
 }
 
+#define ITER 1048576  // 1Mi or 1<<20
+
+uint32_t crc32c_table[256] = {
+  0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,
+  0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,
+  0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
+  0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
+  0x1db71064, 0x6ab020f2, 0xf3b97148, 0x84be41de,
+  0x1adad47d, 0x6ddde4eb, 0xf4d4b551, 0x83d385c7,
+  0x136c9856, 0x646ba8c0, 0xfd62f97a, 0x8a65c9ec,
+  0x14015c4f, 0x63066cd9, 0xfa0f3d63, 0x8d080df5,
+  0x3b6e20c8, 0x4c69105e, 0xd56041e4, 0xa2677172,
+  0x3c03e4d1, 0x4b04d447, 0xd20d85fd, 0xa50ab56b,
+  0x35b5a8fa, 0x42b2986c, 0xdbbbc9d6, 0xacbcf940,
+  0x32d86ce3, 0x45df5c75, 0xdcd60dcf, 0xabd13d59,
+  0x26d930ac, 0x51de003a, 0xc8d75180, 0xbfd06116,
+  0x21b4f4b5, 0x56b3c423, 0xcfba9599, 0xb8bda50f,
+  0x2802b89e, 0x5f058808, 0xc60cd9b2, 0xb10be924,
+  0x2f6f7c87, 0x58684c11, 0xc1611dab, 0xb6662d3d,
+  0x76dc4190, 0x01db7106, 0x98d220bc, 0xefd5102a,
+  0x71b18589, 0x06b6b51f, 0x9fbfe4a5, 0xe8b8d433,
+  0x7807c9a2, 0x0f00f934, 0x9609a88e, 0xe10e9818,
+  0x7f6a0dbb, 0x086d3d2d, 0x91646c97, 0xe6635c01,
+  0x6b6b51f4, 0x1c6c6162, 0x856530d8, 0xf262004e,
+  0x6c0695ed, 0x1b01a57b, 0x8208f4c1, 0xf50fc457,
+  0x65b0d9c6, 0x12b7e950, 0x8bbeb8ea, 0xfcb9887c,
+  0x62dd1ddf, 0x15da2d49, 0x8cd37cf3, 0xfbd44c65,
+  0x4db26158, 0x3ab551ce, 0xa3bc0074, 0xd4bb30e2,
+  0x4adfa541, 0x3dd895d7, 0xa4d1c46d, 0xd3d6f4fb,
+  0x4369e96a, 0x346ed9fc, 0xad678846, 0xda60b8d0,
+  0x44042d73, 0x33031de5, 0xaa0a4c5f, 0xdd0d7cc9,
+  0x5005713c, 0x270241aa, 0xbe0b1010, 0xc90c2086,
+  0x5768b525, 0x206f85b3, 0xb966d409, 0xce61e49f,
+  0x5edef90e, 0x29d9c998, 0xb0d09822, 0xc7d7a8b4,
+  0x59b33d17, 0x2eb40d81, 0xb7bd5c3b, 0xc0ba6cad,
+  0xedb88320, 0x9abfb3b6, 0x03b6e20c, 0x74b1d29a,
+  0xead54739, 0x9dd277af, 0x04db2615, 0x73dc1683,
+  0xe3630b12, 0x94643b84, 0x0d6d6a3e, 0x7a6a5aa8,
+  0xe40ecf0b, 0x9309ff9d, 0x0a00ae27, 0x7d079eb1,
+  0xf00f9344, 0x8708a3d2, 0x1e01f268, 0x6906c2fe,
+  0xf762575d, 0x806567cb, 0x196c3671, 0x6e6b06e7,
+  0xfed41b76, 0x89d32be0, 0x10da7a5a, 0x67dd4acc,
+  0xf9b9df6f, 0x8ebeeff9, 0x17b7be43, 0x60b08ed5,
+  0xd6d6a3e8, 0xa1d1937e, 0x38d8c2c4, 0x4fdff252,
+  0xd1bb67f1, 0xa6bc5767, 0x3fb506dd, 0x48b2364b,
+  0xd80d2bda, 0xaf0a1b4c, 0x36034af6, 0x41047a60,
+  0xdf60efc3, 0xa867df55, 0x316e8eef, 0x4669be79,
+  0xcb61b38c, 0xbc66831a, 0x256fd2a0, 0x5268e236,
+  0xcc0c7795, 0xbb0b4703, 0x220216b9, 0x5505262f,
+  0xc5ba3bbe, 0xb2bd0b28, 0x2bb45a92, 0x5cb36a04,
+  0xc2d7ffa7, 0xb5d0cf31, 0x2cd99e8b, 0x5bdeae1d,
+  0x9b64c2b0, 0xec63f226, 0x756aa39c, 0x026d930a,
+  0x9c0906a9, 0xeb0e363f, 0x72076785, 0x05005713,
+  0x95bf4a82, 0xe2b87a14, 0x7bb12bae, 0x0cb61b38,
+  0x92d28e9b, 0xe5d5be0d, 0x7cdcefb7, 0x0bdbdf21,
+  0x86d3d2d4, 0xf1d4e242, 0x68ddb3f8, 0x1fda836e,
+  0x81be16cd, 0xf6b9265b, 0x6fb077e1, 0x18b74777,
+  0x88085ae6, 0xff0f6a70, 0x66063bca, 0x11010b5c,
+  0x8f659eff, 0xf862ae69, 0x616bffd3, 0x166ccf45,
+  0xa00ae278, 0xd70dd2ee, 0x4e048354, 0x3903b3c2,
+  0xa7672661, 0xd06016f7, 0x4969474d, 0x3e6e77db,
+  0xaed16a4a, 0xd9d65adc, 0x40df0b66, 0x37d83bf0,
+  0xa9bcae53, 0xdebb9ec5, 0x47b2cf7f, 0x30b5ffe9,
+  0xbdbdf21c, 0xcabac28a, 0x53b39330, 0x24b4a3a6,
+  0xbad03605, 0xcdd70693, 0x54de5729, 0x23d967bf,
+  0xb3667a2e, 0xc4614ab8, 0x5d681b02, 0x2a6f2b94,
+  0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
+};
+
+void crc32p(uint32_t* in, uint32_t* out) { // CRC32-Pointer
+#if defined(__aarch64__) && defined(__ARM_FEATURE_CRC32)
+  __asm__("crc32w %w0,%w0,%w1\n":"+r"(*out) : "r"(*in));
+#else
+  *out = crc32c_table[(*in) & 0xff] ^ ((*in) >> 8);
+  *out = crc32c_table[(*out) & 0xff] ^ ((*out) >> 8);
+  *out = crc32c_table[(*out) & 0xff] ^ ((*out) >> 8);
+  *out = crc32c_table[(*out) & 0xff] ^ ((*out) >> 8);
+#endif
+}
+
 bool Blockchain::getBlockLongHash(Crypto::cn_context &context, const Block& b, Crypto::Hash& res) {
+  // Sanity Check
   if (b.majorVersion < CryptoNote::BLOCK_MAJOR_VERSION_5) {
     return get_block_longhash(context, b, res);
   }
 
+  // Phase 0: Initialisation
   BinaryArray bd, pot;
   if (!get_block_hashing_blob(b, bd)) {
     logger(ERROR, BRIGHT_RED) << "Failed to get_block_hashing_blob in getBlockLongHash";
     return false;
   }
 
-  // Phase 1
+  // The very different style is caused by a different programmer (https://github.com/ClashLuke/).
+  // Variable Initialisation
+  Crypto::Hash hashInit, hashIter0, hashIter1;
+  uint32_t currentHeight = boost::get<BaseInput>(b.baseTransaction.inputs[0]).blockIndex;
+  uint32_t maxHeight = std::min<uint32_t>(m_blocks.size() - 1, currentHeight - 1 - m_currency.minedMoneyUnlockWindow());
+  uint8_t*  hashi0_8 = hashIter0.data;  // reinterpret_cast<uint8_t*>(&hashIter0);
+  uint64_t* hashi0_64_0 = reinterpret_cast<uint64_t*>(&hashIter0);
+  uint64_t* hashi0_64_1 = &(reinterpret_cast<uint64_t*>(&hashIter0))[1];
+  uint64_t* hashi0_64_2 = &(reinterpret_cast<uint64_t*>(&hashIter0))[2];
+  uint64_t* hashi0_64_3 = &(reinterpret_cast<uint64_t*>(&hashIter0))[3];
+  uint64_t* hashi1_64_0 = reinterpret_cast<uint64_t*>(&hashIter1);
+  uint64_t* hashi1_64_1 = &(reinterpret_cast<uint64_t*>(&hashIter1))[1];
+  uint64_t* hashi1_64_2 = &(reinterpret_cast<uint64_t*>(&hashIter1))[2];
+  uint64_t* hashi1_64_3 = &(reinterpret_cast<uint64_t*>(&hashIter1))[3];
+  uint32_t* hasht_32 = reinterpret_cast<uint32_t*>(&hashInit);
+  uint64_t* hasht_64_0 = reinterpret_cast<uint64_t*>(&hashInit);
+  uint64_t* hasht_64_1 = &(reinterpret_cast<uint64_t*>(&hashInit))[1];
+  uint64_t* hasht_64_2 = &(reinterpret_cast<uint64_t*>(&hashInit))[2];
+  uint64_t* hasht_64_3 = &(reinterpret_cast<uint64_t*>(&hashInit))[3];
+  uint32_t  height_0 = 0;
+  uint32_t  height_1 = 0;
+  uint32_t  height_2 = 0;
+  uint32_t  height_3 = 0;
+  uint32_t  height_4 = 0;
+  uint32_t  height_5 = 0;
+  uint32_t  height_6 = 0;
+  uint32_t  height_7 = 0;
 
-  Crypto::Hash hash_1, hash_2;
+  // Phase 1: Hashing the current blockdata
+  cn_fast_hash(bd.data(), bd.size(), hashInit);
 
-  // Hashing the current blockdata (preprocessing it)
-  cn_fast_hash(bd.data(), bd.size(), hash_1);
-  
+  // Phase 2: Performing ASIC, GPU, Botnet resistant (assymetric) hash
+  // Necessary to have a constant, to fight those in the beginning
+  // when a chain is relatively small; and in the end, when a chain grows
+  // slower than hardware improves.
+  // TODO
+
+  // Phase 3: Blockchain Specific Operations
+  // Get the corresponding 32 blocks from blockchain based on preparatory hash_1
+  // and throw them into the pot too
+  for (uint32_t i = 0; i < ITER; i++) {
+    crc32p(hasht_32, &height_0); crc32p(&hasht_32[1], &height_1);
+    crc32p(&hasht_32[2], &height_2); crc32p(&hasht_32[3], &height_3);
+    crc32p(&hasht_32[4], &height_4); crc32p(&hasht_32[5], &height_5);
+    crc32p(&hasht_32[6], &height_6); crc32p(&hasht_32[7], &height_7);
+    height_0 %= maxHeight; height_1 %= maxHeight;
+    height_2 %= maxHeight; height_3 %= maxHeight;
+    height_4 %= maxHeight; height_5 %= maxHeight;
+    height_6 %= maxHeight; height_7 %= maxHeight;
+    hashIter0 = getBlockIdByHeight(height_0);
+    hashIter1 = getBlockIdByHeight(height_1);
+    *hasht_64_0 += *hashi0_64_0; *hasht_64_1 += *hashi0_64_1;
+    *hasht_64_2 += *hashi0_64_2; *hasht_64_3 += *hashi0_64_3;
+    *hasht_64_0 += *hashi1_64_0; *hasht_64_1 += *hashi1_64_1;
+    *hasht_64_2 += *hashi1_64_2; *hasht_64_3 += *hashi1_64_3;
+    hashIter0 = getBlockIdByHeight(height_2);
+    hashIter1 = getBlockIdByHeight(height_3);
+    *hasht_64_0 /= *hashi0_64_0; *hasht_64_1 /= *hashi0_64_1;
+    *hasht_64_2 /= *hashi0_64_2; *hasht_64_3 /= *hashi0_64_3;
+    *hasht_64_0 *= *hashi1_64_0; *hasht_64_1 *= *hashi1_64_1;
+    *hasht_64_2 *= *hashi1_64_2; *hasht_64_3 *= *hashi1_64_3;
+    hashIter0 = getBlockIdByHeight(height_4);
+    hashIter1 = getBlockIdByHeight(height_5);
+    *hasht_64_0 -= *hashi0_64_0; *hasht_64_1 -= *hashi0_64_1;
+    *hasht_64_2 -= *hashi0_64_2; *hasht_64_3 -= *hashi0_64_3;
+    *hasht_64_0 -= *hashi1_64_0; *hasht_64_1 -= *hashi1_64_1;
+    *hasht_64_2 -= *hashi1_64_2; *hasht_64_3 -= *hashi1_64_3;
+    hashIter0 = getBlockIdByHeight(height_6);
+    hashIter1 = getBlockIdByHeight(height_7);
+    *hasht_64_0 ^= *hashi0_64_0; *hasht_64_1 ^= *hashi0_64_1;
+    *hasht_64_2 ^= *hashi0_64_2; *hasht_64_3 ^= *hashi0_64_3;
+    *hasht_64_0 ^= *hashi1_64_0; *hasht_64_1 ^= *hashi1_64_1;
+    *hasht_64_2 ^= *hashi1_64_2; *hasht_64_3 ^= *hashi1_64_3;
+    height_0 &= 0x3F;     height_1 &= 0x3F;
+    height_2 &= 0x3F;     height_3 &= 0x3F;
+    height_4 = 0x40;      height_5 = 0x40;
+    height_6 = 0x40;      height_7 = 0x40;
+    height_4 -= height_0; height_5 -= height_1;
+    height_6 -= height_2; height_7 -= height_3;
+    *hasht_64_0 = ((uint64_t)hasht_64_0 << (uint64_t)height_0) | ((uint64_t)hasht_64_0 << (uint64_t)height_4);
+    *hasht_64_1 = ((uint64_t)hasht_64_1 << (uint64_t)height_1) | ((uint64_t)hasht_64_1 << (uint64_t)height_5);
+    *hasht_64_2 = ((uint64_t)hasht_64_2 << (uint64_t)height_2) | ((uint64_t)hasht_64_2 << (uint64_t)height_6);
+    *hasht_64_3 = ((uint64_t)hasht_64_3 << (uint64_t)height_3) | ((uint64_t)hasht_64_3 << (uint64_t)height_7);
+  }
+
+  //res = hashInit;
+
   // Phase 2
 
+  Crypto::Hash hash_1, hash_2;
+    
+  hash_1 = hashInit;
+  
   // throw our block into common pot
   pot.insert(std::end(pot), std::begin(bd), std::end(bd));
 
-  // Get the corresponding 32 blocks from blockchain based on preparatory hash_1
+  // Get the corresponding 8 blocks from blockchain based on phase 1
   // and throw them into the pot too
 
-  uint32_t currentHeight = boost::get<BaseInput>(b.baseTransaction.inputs[0]).blockIndex;
-  uint32_t maxHeight = std::min<uint32_t>(m_blocks.size(), currentHeight - 1 - m_currency.minedMoneyUnlockWindow());
   std::vector<uint64_t> heights;
 
-  fillHeights(hash_1.data, sizeof(hash_1), maxHeight, heights, 32);
+  heights.push_back(height_0);
+  heights.push_back(height_1);
+  heights.push_back(height_2);
+  heights.push_back(height_3);
+  heights.push_back(height_4);
+  heights.push_back(height_5);
+  heights.push_back(height_6);
+  heights.push_back(height_7);
 
-  for (auto i = 0; i < 32; ++i) {
+  //fillHeights(hash_1.data, sizeof(hash_1), maxHeight, heights, 32);
+
+  for (auto i = 0; i < 8; ++i) {
+
     Crypto::Hash hash_i = getBlockIdByHeight(static_cast<uint32_t>(heights[i]));
 
     Block bl;
@@ -1273,11 +1447,11 @@ bool Blockchain::getBlockLongHash(Crypto::cn_context &context, const Block& b, C
 
   // Phase 3
 
-  uint32_t m_cost = currentHeight > 813 ? 512 : 128;
+  uint32_t m_cost = 1024;
   uint32_t lanes = 2;
   uint32_t t_cost = 2;
 
-  // stir the pot - hashing the 1 + 32 blocks as one continuous data, salt is hash_1
+  // stir the pot - hashing the 1 + 8 blocks as one continuous data, salt is hash_1
   Crypto::argon2d_hash(pot.data(), pot.size(), hash_1.data, sizeof(hash_1), m_cost, lanes, t_cost, hash_2);
 
   //logger(INFO, BRIGHT_GREEN) << "getBlockLongHash: " << res;
