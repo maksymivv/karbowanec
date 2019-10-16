seredat/karbowanez

Add support SSL for RpcServer in PaymentGate

src/PaymentGateService/PaymentGateService.cpp
@@ -20,6 +20,7 @@
 #include "PaymentGateService.h"
 
 #include <future>
+#include <boost/filesystem.hpp>
 
 #include "Common/SignalHandler.h"
 #include "InProcessNode/InProcessNode.h"
@@ -45,6 +46,33 @@
 #endif
 
 using namespace PaymentService;
+
+bool validateSertPath(const std::string& rootPath,
+                      const std::string& config_chain_file,
+                      const std::string& config_key_file,
+                      const std::string& config_dh_file,
+                      std::string& chain_file,
+                      std::string& key_file,
+                      std::string& dh_file) {
+  bool res = false;
+  boost::system::error_code ec;
+  boost::filesystem::path data_dir_path(rootPath);
+  boost::filesystem::path chain_file_path(config_chain_file);
+  boost::filesystem::path key_file_path(config_key_file);
+  boost::filesystem::path dh_file_path(config_dh_file);
+  if (!chain_file_path.has_parent_path()) chain_file_path = data_dir_path / chain_file_path;
+  if (!key_file_path.has_parent_path()) key_file_path = data_dir_path / key_file_path;
+  if (!dh_file_path.has_parent_path()) dh_file_path = data_dir_path / dh_file_path;
+  if (boost::filesystem::exists(chain_file_path, ec) &&
+      boost::filesystem::exists(key_file_path, ec) &&
+      boost::filesystem::exists(dh_file_path, ec)) {
+        chain_file = boost::filesystem::canonical(chain_file_path).string();
+        key_file = boost::filesystem::canonical(key_file_path).string();
+        dh_file = boost::filesystem::canonical(dh_file_path).string();
+        res = true;
+  }
+  return res;
+}
 
 void changeDirectory(const std::string& path) {
   if (chdir(path.c_str())) {
@@ -209,9 +237,32 @@ void PaymentGateService::runInProcess(Logging::LoggerRef& log) {
     throw std::system_error(ec);
   }
 
+  bool rpc_run_ssl = false;
+  std::string rpc_chain_file = "";
+  std::string rpc_key_file = "";
+  std::string rpc_dh_file = "";
+
+  if (config.remoteNodeConfig.m_enable_ssl) {
+    if (validateSertPath(config.coreConfig.configFolder,
+        config.remoteNodeConfig.m_chain_file,
+        config.remoteNodeConfig.m_key_file,
+        config.remoteNodeConfig.m_dh_file,
+        rpc_chain_file,
+        rpc_key_file,
+        rpc_dh_file)){
+          rpcServer.setCerts(rpc_chain_file, rpc_key_file, rpc_dh_file);
+          rpc_run_ssl = true;
+    } else {
+      log((Logging::Level) Logging::ERROR, Logging::BRIGHT_RED) << "Start RPC SSL server was canceled because certificate file(s) could not be found" << std::endl;
+    }
+  }
+
   log(Logging::INFO) << "Starting core rpc server on "
 	  << config.remoteNodeConfig.daemonHost << ":" << config.remoteNodeConfig.daemonPort;
-  rpcServer.start(config.remoteNodeConfig.daemonHost, config.remoteNodeConfig.daemonPort);
+  rpcServer.start(config.remoteNodeConfig.daemonHost,
+                  config.remoteNodeConfig.daemonPort,
+                  config.remoteNodeConfig.daemonPortSSL,
+                  rpc_run_ssl);
   log(Logging::INFO) << "Core rpc server started ok";
 
   log(Logging::INFO) << "Spawning p2p server";
@@ -274,8 +325,35 @@ void PaymentGateService::runWalletService(const CryptoNote::Currency& currency, 
       std::cout << "Address: " << address << std::endl;
     }
   } else {
+
     PaymentService::PaymentServiceJsonRpcServer rpcServer(*dispatcher, *stopEvent, *service, logger);
-    rpcServer.start(config.gateConfiguration.bindAddress, config.gateConfiguration.bindPort, config.gateConfiguration.m_rpcUser, config.gateConfiguration.m_rpcPassword);
+
+    bool rpc_run_ssl = false;
+    std::string rpc_chain_file = "";
+    std::string rpc_key_file = "";
+    std::string rpc_dh_file = "";
+
+    if (config.gateConfiguration.m_enable_ssl) {
+        if (validateSertPath(config.coreConfig.configFolder,
+            config.gateConfiguration.m_chain_file,
+            config.gateConfiguration.m_key_file,
+            config.gateConfiguration.m_dh_file,
+            rpc_chain_file,
+            rpc_key_file,
+            rpc_dh_file)){
+            rpcServer.setCerts(rpc_chain_file, rpc_key_file, rpc_dh_file);
+            rpc_run_ssl = true;
+        } else {
+           Logging::LoggerRef(logger, "PaymentGateService")(Logging::ERROR, Logging::BRIGHT_RED) << "Start JSON-RPC SSL server was canceled because certificate file(s) could not be found" << std::endl;
+        }
+    }
+
+    rpcServer.start(config.gateConfiguration.bindAddress,
+                    config.gateConfiguration.bindPort,
+                    config.gateConfiguration.bindPortSSL,
+                    rpc_run_ssl,
+                    config.gateConfiguration.m_rpcUser,
+                    config.gateConfiguration.m_rpcPassword);
 
     Logging::LoggerRef(logger, "PaymentGateService")(Logging::INFO, Logging::BRIGHT_WHITE) << "JSON-RPC server stopped, stopping wallet service...";
 

maksymivv/karbowanec

src/PaymentGateService/PaymentGateService.cpp
@@ -21,6 +21,7 @@
 #include "PaymentGateService.h"
 
 #include <future>
+#include <boost/filesystem.hpp>
 
 #include "Common/SignalHandler.h"
 #include "InProcessNode/InProcessNode.h"
@@ -47,6 +48,33 @@
 #endif
 
 using namespace PaymentService;
+
+bool validateSertPath(const std::string& rootPath,
+                      const std::string& config_chain_file,
+                      const std::string& config_key_file,
+                      const std::string& config_dh_file,
+                      std::string& chain_file,
+                      std::string& key_file,
+                      std::string& dh_file) {
+  bool res = false;
+  boost::system::error_code ec;
+  boost::filesystem::path data_dir_path(rootPath);
+  boost::filesystem::path chain_file_path(config_chain_file);
+  boost::filesystem::path key_file_path(config_key_file);
+  boost::filesystem::path dh_file_path(config_dh_file);
+  if (!chain_file_path.has_parent_path()) chain_file_path = data_dir_path / chain_file_path;
+  if (!key_file_path.has_parent_path()) key_file_path = data_dir_path / key_file_path;
+  if (!dh_file_path.has_parent_path()) dh_file_path = data_dir_path / dh_file_path;
+  if (boost::filesystem::exists(chain_file_path, ec) &&
+      boost::filesystem::exists(key_file_path, ec) &&
+      boost::filesystem::exists(dh_file_path, ec)) {
+        chain_file = boost::filesystem::canonical(chain_file_path).string();
+        key_file = boost::filesystem::canonical(key_file_path).string();
+        dh_file = boost::filesystem::canonical(dh_file_path).string();
+        res = true;
+  }
+  return res;
+}
 
 void changeDirectory(const std::string& path) {
   if (chdir(path.c_str())) {
@@ -215,9 +243,32 @@ void PaymentGateService::runInProcess(Logging::LoggerRef& log) {
     throw std::system_error(ec);
   }
 
+  bool rpc_run_ssl = false;
+  std::string rpc_chain_file = "";
+  std::string rpc_key_file = "";
+  std::string rpc_dh_file = "";
+
+  if (config.remoteNodeConfig.m_enable_ssl) {
+    if (validateSertPath(config.coreConfig.configFolder,
+        config.remoteNodeConfig.m_chain_file,
+        config.remoteNodeConfig.m_key_file,
+        config.remoteNodeConfig.m_dh_file,
+        rpc_chain_file,
+        rpc_key_file,
+        rpc_dh_file)) {
+      rpcServer.setCerts(rpc_chain_file, rpc_key_file, rpc_dh_file);
+      rpc_run_ssl = true;
+    } else {
+      log((Logging::Level) Logging::ERROR, Logging::BRIGHT_RED) << "Start RPC SSL server was canceled because certificate file(s) could not be found" << std::endl;
+    }
+  }
+
   log(Logging::INFO) << "Starting core rpc server on "
-	  << config.remoteNodeConfig.daemonHost << ":" << config.remoteNodeConfig.daemonPort;
-  rpcServer.start(config.remoteNodeConfig.daemonHost, config.remoteNodeConfig.daemonPort);
+	  << config.remoteNodeConfig.m_daemon_host << ":" << config.remoteNodeConfig.m_daemon_port;
+  rpcServer.start(config.remoteNodeConfig.m_daemon_host,
+                  config.remoteNodeConfig.m_daemon_port,
+                  config.remoteNodeConfig.m_daemon_port_ssl,
+                  rpc_run_ssl);
   log(Logging::INFO) << "Core rpc server started ok";
 
   log(Logging::INFO) << "Spawning p2p server";
@@ -249,8 +300,8 @@ void PaymentGateService::runRpcProxy(Logging::LoggerRef& log) {
 
   std::unique_ptr<CryptoNote::INode> node(
     PaymentService::NodeFactory::createNode(
-      config.remoteNodeConfig.daemonHost,
-      config.remoteNodeConfig.daemonPort));
+      config.remoteNodeConfig.m_daemon_host,
+      config.remoteNodeConfig.m_daemon_port));
 
   runWalletService(currency, *node);
 }
@@ -280,8 +331,35 @@ void PaymentGateService::runWalletService(const CryptoNote::Currency& currency, 
       std::cout << "Address: " << address << std::endl;
     }
   } else {
+
     PaymentService::PaymentServiceJsonRpcServer rpcServer(*dispatcher, *stopEvent, *service, logger);
-    rpcServer.start(config.gateConfiguration.bindAddress, config.gateConfiguration.bindPort, config.gateConfiguration.m_rpcUser, config.gateConfiguration.m_rpcPassword);
+
+    bool rpc_run_ssl = false;
+    std::string rpc_chain_file = "";
+    std::string rpc_key_file = "";
+    std::string rpc_dh_file = "";
+
+    if (config.gateConfiguration.m_enable_ssl) {
+        if (validateSertPath(config.coreConfig.configFolder,
+            config.gateConfiguration.m_chain_file,
+            config.gateConfiguration.m_key_file,
+            config.gateConfiguration.m_dh_file,
+            rpc_chain_file,
+            rpc_key_file,
+            rpc_dh_file)){
+            rpcServer.setCerts(rpc_chain_file, rpc_key_file, rpc_dh_file);
+            rpc_run_ssl = true;
+        } else {
+           Logging::LoggerRef(logger, "PaymentGateService")(Logging::ERROR, Logging::BRIGHT_RED) << "Start JSON-RPC SSL server was canceled because certificate file(s) could not be found" << std::endl;
+        }
+    }
+
+    rpcServer.start(config.gateConfiguration.m_bind_address,
+                    config.gateConfiguration.m_bind_port,
+                    config.gateConfiguration.m_bind_port_ssl,
+                    rpc_run_ssl,
+                    config.gateConfiguration.m_rpcUser,
+                    config.gateConfiguration.m_rpcPassword);
 
     Logging::LoggerRef(logger, "PaymentGateService")(Logging::INFO, Logging::BRIGHT_WHITE) << "JSON-RPC server stopped, stopping wallet service...";
 