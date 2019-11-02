5f9584da5c3defa8764c1bcb7a930d29685f27be
Vasya Ichkoff <vasyaichkoff@gmail.com>
2019-11-02 14:34:55 +0200

    Added anonymous function add_windows_root_certs (HttpClient)

--- src/Rpc/HttpClient.cpp
+++ src/Rpc/HttpClient.cpp
@@ -21,6 +21,38 @@
 #include <System/Ipv4Resolver.h>
 #include <System/Ipv4Address.h>
 #include <System/TcpConnector.h>
+#include <System/SocketStream.h>
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+#include <boost/asio/ssl/stream.hpp>
+#if defined(WIN32)
+#include <wincrypt.h>
+#endif
+
+using boost::asio::ip::tcp;
+
+
+#if defined(WIN32)
+void add_windows_root_certs(boost::asio::ssl::context &ctx) {
+  HCERTSTORE hStore = CertOpenSystemStore(0, "ROOT");
+  if (hStore != NULL) {
+    X509_STORE *store = X509_STORE_new();
+    PCCERT_CONTEXT pContext = NULL;
+    while ((pContext = CertEnumCertificatesInStore(hStore, pContext)) != NULL) {
+      X509 *x509 = d2i_X509(NULL,
+                            (const unsigned char **) &pContext->pbCertEncoded,
+                            pContext->cbCertEncoded);
+      if (x509 != NULL) {
+        X509_STORE_add_cert(store, x509);
+        X509_free(x509);
+      }
+    }
+    CertFreeCertificateContext(pContext);
+    CertCloseStore(hStore, 0);
+    SSL_CTX_set_cert_store(ctx.native_handle(), store);
+  }
+}
+#endif
 
 namespace CryptoNote {
 
