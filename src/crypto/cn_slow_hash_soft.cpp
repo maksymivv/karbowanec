993bf9130441592108aa7ea5572d1d33f7be8e72
aiwe <maksymivv@gmail.com>
2019-12-26 22:38:14 +0200

    Properly set up logic

--- src/crypto/cn_slow_hash_soft.cpp
+++ src/crypto/cn_slow_hash_soft.cpp
@@ -510,16 +510,16 @@ void cn_slow_hash<MEMORY, ITER, POW_VER>::software_hash(const void* in, size_t l
 			{
 				__m128i cx_xmm = _mm_set_epi64x(cx.v64x1, cx.v64x0);
 				cx_xmm = _mm_xor_si128(cx_xmm, bx_xmm);
-#if !defined(__arm__) || !defined(__aarch64__)
+#if defined(__arm__) || defined(__aarch64__)
+        __m128d da = _mm_cvtepi32_ps(cx_xmm);
+        __m128d db = _mm_cvtepi32_ps(_mm_shuffle_epi32_default(cx_xmm, _MM_SHUFFLE(0, 1, 2, 3)));
+        da = _mm_mul_ps(da, db);
+        __m128i dx = _mm_castps_si128(da);
+#else
 				__m128d da = _mm_cvtepi32_pd(cx_xmm);
 				__m128d db = _mm_cvtepi32_pd(_mm_shuffle_epi32(cx_xmm, _MM_SHUFFLE(0,1,2,3)));
 				da = _mm_mul_pd(da, db);
 				__m128i dx = _mm_castpd_si128(da);
-#else
-				__m128d da = _mm_cvtepi32_ps(cx_xmm);
-				__m128d db = _mm_cvtepi32_ps(_mm_shuffle_epi32_default(cx_xmm, _MM_SHUFFLE(0,1,2,3)));
-				da = _mm_mul_ps(da, db);
-				__m128i dx = _mm_castps_si128(da);
 #endif
 				cx.v64x0 = _mm_cvtsi128_si64(dx);
 				dx = _mm_shuffle_epi32(dx, _MM_SHUFFLE(1,0,3,2));
@@ -567,16 +567,16 @@ void cn_slow_hash<MEMORY, ITER, POW_VER>::software_hash(const void* in, size_t l
 			{
 				__m128i bx_xmm = _mm_set_epi64x(bx.v64x1, bx.v64x0);
 				bx_xmm = _mm_xor_si128(bx_xmm, cx_xmm);
-#if !defined(__arm__) || !defined(__aarch64__)
-				__m128d da = _mm_cvtepi32_pd(bx_xmm);
-				__m128d db = _mm_cvtepi32_pd(_mm_shuffle_epi32(bx_xmm, _MM_SHUFFLE(0,1,2,3)));
-        da = _mm_mul_pd(da, db);
-        __m128i dx = _mm_castpd_si128(da);
-#else
+#if defined(__arm__) || defined(__aarch64__)
         __m128d da = _mm_cvtepi32_ps(bx_xmm);
         __m128d db = _mm_cvtepi32_ps(_mm_shuffle_epi32(bx_xmm, _MM_SHUFFLE(0, 1, 2, 3)));
-				da = _mm_mul_ps(da, db);
-				__m128i dx = _mm_castps_si128(da);
+        da = _mm_mul_ps(da, db);
+        __m128i dx = _mm_castps_si128(da);
+#else
+        __m128d da = _mm_cvtepi32_pd(bx_xmm);
+        __m128d db = _mm_cvtepi32_pd(_mm_shuffle_epi32(bx_xmm, _MM_SHUFFLE(0, 1, 2, 3)));
+        da = _mm_mul_pd(da, db);
+        __m128i dx = _mm_castpd_si128(da);
 #endif
 				bx.v64x0 = _mm_cvtsi128_si64(dx);
 				dx = _mm_shuffle_epi32(dx, _MM_SHUFFLE(1,0,3,2));
