418f1721f0190b11153212457024d51d2a14933c
aiwe <maksymivv@gmail.com>
2020-01-01 20:57:44 +0200

    Parse stake amount in miner config

--- src/CryptoNoteCore/MinerConfig.cpp
+++ src/CryptoNoteCore/MinerConfig.cpp
@@ -18,6 +18,7 @@
 #include "MinerConfig.h"
 
 #include "Common/CommandLine.h"
+#include "Common/FormatTools.h"
 
 namespace CryptoNote {
 
@@ -25,7 +26,7 @@ namespace {
 const command_line::arg_descriptor<std::string> arg_extra_messages  = {"extra-messages-file", "Specify file for extra messages to include into coinbase transactions", "", true};
 const command_line::arg_descriptor<std::string> arg_start_mining    = {"start-mining", "Specify wallet address to mining for", "", true};
 const command_line::arg_descriptor<uint32_t>    arg_mining_threads  = {"mining-threads", "Specify mining threads count", 0, true};
-const command_line::arg_descriptor<uint32_t>    arg_stake_amount    = {"stake", "Specify wanted stake amount", 0, true};
+const command_line::arg_descriptor<std::string> arg_stake_amount    = {"stake", "Specify wanted stake amount", "", true};
 }
 
 MinerConfig::MinerConfig() {
@@ -53,7 +54,7 @@ void MinerConfig::init(const boost::program_options::variables_map& options) {
   }
 
   if (command_line::has_arg(options, arg_stake_amount)) {
-    stakeAmount = command_line::get_arg(options, arg_stake_amount);
+    Common::Format::parseAmount(command_line::get_arg(options, arg_stake_amount), stakeAmount);
   }
 }
 
