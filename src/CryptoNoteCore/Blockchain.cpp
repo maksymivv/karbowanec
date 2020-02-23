b547d39ad3747953bf83c74332909e9b08dd45d7
aiwe <maksymivv@gmail.com>
2020-02-23 13:07:42 +0200

    Move constanst to DB header

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -338,20 +338,6 @@ private:
   Crypto::Hash m_lastBlockHash;
 };
 
-const std::string version_current = "1"; // increment when making incompatible changes to indexes
-
-// use suffixes so all keys related to the same block are close to each other in DB
-static const std::string BLOCK_PREFIX = "b";
-static const std::string BLOCK_SUFFIX = "b";
-static const std::string TRANSACTION_PREFIX = "t";
-static const std::string TIP_CHAIN_PREFIX = "c";
-static const std::string TIMESTAMP_INDEX_PREFIX = "T";
-static const std::string TRANSACTIONS_INDEX_PREFIX = "i";
-static const std::string SPENT_KEY_IMAGES_INDEX_PREFIX = "k";
-static const std::string PAYMENT_ID_INDEX_PREFIX = "p";
-static const std::string GENERATED_TRANSACTIONS_INDEX_PREFIX = "g";
-static const std::string ORPHAN_BLOCK_INDEX_PREFIX = "o";
-
 Blockchain::Blockchain(const Currency& currency, tx_memory_pool& tx_pool, ILogger& logger, bool blockchainIndexesEnabled, bool blockchainReadOnly, const std::string& config_folder) :
 logger(logger, "Blockchain"),
 m_currency(currency),
--- src/CryptoNoteCore/DB.hpp
+++ src/CryptoNoteCore/DB.hpp
@@ -8,3 +8,20 @@ namespace Platform {
 typedef DBlmdb DB;
 }
 
+namespace CryptoNote {
+
+  const std::string version_current = "1"; // increment when making incompatible changes to indexes
+
+  // use suffixes so all keys related to the same block are close to each other in DB
+  static const std::string BLOCK_PREFIX = "b";
+  static const std::string BLOCK_SUFFIX = "b";
+  static const std::string TRANSACTION_PREFIX = "t";
+  static const std::string TIP_CHAIN_PREFIX = "c";
+  static const std::string TIMESTAMP_INDEX_PREFIX = "T";
+  static const std::string TRANSACTIONS_INDEX_PREFIX = "i";
+  static const std::string SPENT_KEY_IMAGES_INDEX_PREFIX = "k";
+  static const std::string PAYMENT_ID_INDEX_PREFIX = "p";
+  static const std::string GENERATED_TRANSACTIONS_INDEX_PREFIX = "g";
+  static const std::string ORPHAN_BLOCK_INDEX_PREFIX = "o";
+
+}
