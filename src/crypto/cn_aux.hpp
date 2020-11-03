ec8e347d8e7fc0b9506b52f9d78e1e4be5385fad
aiwe <maksymivv@gmail.com>
2020-11-03 16:14:47 +0200

    Fix warning in cn_aux

--- src/crypto/cn_aux.hpp
+++ src/crypto/cn_aux.hpp
@@ -18,7 +18,7 @@
 #include "sse2neon.h"
 #endif
 
-#ifdef __GNUC__ && !defined(ARM)
+#if defined(__GNUC__) && !defined(ARM)
 #pragma GCC target ("aes,sse2")
 #include <x86intrin.h>
 static inline uint64_t _umul128(uint64_t a, uint64_t b, uint64_t* hi)
